3e74b5671befe0c02b4b07e7dc41e083e85fc79a
YutaIwashina <hal.pw50368@gmail.com>
2017-04-21 19:13:56 +0900

    20170421 課題完成、コメント入力画面にキーボードが隠れないようにする処理を追加

--- Instagram/CommentViewController.swift
+++ Instagram/CommentViewController.swift
@@ -38,13 +38,18 @@ class CommentViewController: UIViewController {
         // name & caption
         self.getCaptionLabel.text = "\(postData.name!) : \(postData.caption!)"
         
-//        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
-//        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-//        self.view.addGestureRecognizer(tapGesture)
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    // キーボードを閉じる
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     /* コメント保存処理 -------------------------------------------------------------------------------------------------*/
-    @IBAction func commentPostButton(_ sender: Any) {
+    @IBAction func postButton(_ sender: Any) {
         // コメント入力欄にコメントが入っているかの条件分岐
         if inputCommentTextField.text != "" {
             // コメントしているユーザーの表示名を取得
@@ -66,23 +71,56 @@ class CommentViewController: UIViewController {
     }
     /* コメント保存処理 end----------------------------------------------------------------------------------------------*/
     
-//    // キーボードを閉じる
-//    func dismissKeyboard() {
-//        view.endEditing(true)
-//    }
-    
-    //改行ボタンが押された際に呼ばれる.
+    /* UITextFieldタップ時のキーボードに関する処理 -------------------------------------------------*/
+    // リターンボタン（完了）が押された際に呼ばれる。キーボードを隠す
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         
         return true
     }
+    // UITextFieldが編集された直後に呼ばれる.
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        inputCommentTextField = textField
+        return true
+    }
+    
+    // スクロールビューをキーボードの分だけずらす処理
+    func handleKeyboardWillShowNotification(_ notification: Notification) {
+        
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let myBoundSize: CGSize = UIScreen.main.bounds.size
+        
+        let txtLimit = inputCommentTextField.frame.origin.y + inputCommentTextField.frame.height + 8.0
+        let kbdLimit = myBoundSize.height - keyboardScreenEndFrame.size.height
+        
+        if txtLimit >= kbdLimit {
+            scvBackGround.contentOffset.y = txtLimit - kbdLimit
+        }
+    }
+    
+    // スクロールビューをずらした分を戻す処理
+    func handleKeyboardWillHideNotification(_ notification: Notification) {
+        scvBackGround.contentOffset.y = 0
+    }
+    
+    // キーボードを出し入れする処理
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(self.handleKeyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(self.handleKeyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    /* UITextFieldタップ時のキーボードをスクロールする処理 end----------------------------------------------*/
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // HomeViewControllerに戻る
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
